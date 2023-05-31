6a4fa6eef16f2560d9e95bcd019c960790a19091
Jason <Jason06052005@gmail.com>
2023-05-30 19:44:29 -0700

    create user repository

---
+++ repository/user_repository.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm.session import Session
+from db.user_db import UserDb
+
+
+def add_user(db: Session, _username, _email, _role):
+    new_user= UserDb(username=_username, email=_email, role=_role)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+def add_user_td(db: Session):
+    add_user(db, "darth.vader", "darth.vader@gmail.com", "villian")
+    add_user(db,"thor.odinson", "thor@gmail.com", "hero" )
+    add_user(db, "jason.pham", "jason@gmail.com", "student")
