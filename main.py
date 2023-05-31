897fec6b1319d450c037be666f0d1285d0d9e84c
Jason <Jason06052005@gmail.com>
2023-05-30 18:58:11 -0700

    add new health endpoint and test

--- main.py
+++ main.py
@@ -13,3 +13,9 @@ app = FastAPI(
 async def welcome(response: Response):
         response.status_code = status.HTTP_200_OK
         return {"message":  "Hello, welcome to Jason's API"}
+
+
+@app.get("/health", tags=['health'], response_description="Retrieves health status of this application")
+def health(response: Response):
+    response.status_code = status.HTTP_200_OK
+    return {"status": "OK" }
