0fef1a05f64730b35cc11b98e0810ec3b20a09a9
Jason <Jason06052005@gmail.com>
2023-05-31 17:00:42 -0700

    refractor code to handle a parameter that is not found

--- router/users_router.py
+++ router/users_router.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Response, Depends, status
+from fastapi import APIRouter, Response, Depends, status, HTTPException
 from repository import user_repository
 from sqlalchemy.orm import Session
 from db_config import get_db
-from schemas import UserModel
+from schemas import UserModel, MessageModel
 
 router = APIRouter(
     prefix = "/users/v1",
@@ -17,8 +17,15 @@ def get_users(response: Response, db: Session = Depends(get_db)):
     return return_value
 
 
-@router.get("/username}", response_description="Displays user by username", description="Retrieves user by username", response_model=UserModel)
+@router.get("/username}", response_description="Displays user by username", description="Retrieves user by username", response_model=UserModel, responses={404: {"model": MessageModel}})
 def get_by_username(username: str, response: Response, db: Session = Depends(get_db)):
     return_value = user_repository.get_by_username(db, username)
+    
+    
+    if return_value == None:
+        response_text = "username not found. Please check your parameter and try again."
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=response_text)
+    
     response.status_code = status.HTTP_200_OK
     return return_value
+    
